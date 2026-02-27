8a071cf20b24574167cc7d870f2d7bc5e5697eb0
ReaSetilo <202103018@ub.ac.bw>
2026-02-27 02:09:18 +0200

    auth
    auth ui and logic

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Outlet, redirect } from 'react-router'
 import * as Navs from '@syncfusion/ej2-react-navigations'
 import NavItems from 'components/NavItems'
 import MobileSidebar from 'components/MobileSidebar'
+import { account } from '~/appwrite/client'
+import { getExistingUser, storeUserData } from '~/appwrite/auth'
+export async function clientLoader() {
+    try {
+        const user = await account.get();
+
+        if(!user.$id) return redirect('/sign-in');
+
+        const existingUser = await getExistingUser(user.$id);
+
+        if(existingUser?.status === 'user') {
+            return redirect('/');
+        }
+
+        return existingUser?.$id ? existingUser : await storeUserData();
+    } catch (e) {
+        console.log('Error in clientLoader', e)
+        return redirect('/sign-in')
+    }
+}
 
 const AdminLayout = () => {
   return (

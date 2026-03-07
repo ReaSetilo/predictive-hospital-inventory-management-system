9bca7a86b54ffbdd94cf74dae92ec5033bf11628
ReaSetilo <202103018@ub.ac.bw>
2026-03-07 21:03:49 +0200

    Update Header.tsx
    restore header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { useLocation, Link } from 'react-router';
+import { useLocation } from 'react-router';
 import { cn } from '~/lib/utils'
-
-interface Props {
+interface Props{
     title: string;
     description: string;
 }
 const Header = ({title, description}: Props) => {
     const location = useLocation();
-    
-    return (
+  return (
     <header className='header'>
         <article>
             <h1 className={cn('text-dark-100', location.pathname === '/' ? 'text-2xl md:text-4xl font-bold' : 'text-xl md:text-2xl font-semibold' )}>{title}</h1>

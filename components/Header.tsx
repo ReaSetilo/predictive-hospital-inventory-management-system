a86ccef9edb3136a12d7032562f56c5b24d9d8cf
ReaSetilo <202103018@ub.ac.bw>
2026-03-07 20:56:20 +0200

    fix button
    fix button

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,10 +6,8 @@ import {ButtonComponent} from "@syncfusion/ej2-react-buttons";
 interface Props {
     title: string;
     description: string;
-    ctaText?: string;
-    ctaUrl?: string;
 }
-const Header = ({title, description, ctaText, ctaUrl}: Props) => {
+const Header = ({title, description}: Props) => {
     const location = useLocation();
     
     return (
@@ -18,15 +16,6 @@ const Header = ({title, description, ctaText, ctaUrl}: Props) => {
             <h1 className={cn('text-dark-100', location.pathname === '/' ? 'text-2xl md:text-4xl font-bold' : 'text-xl md:text-2xl font-semibold' )}>{title}</h1>
             <p className={cn('text-gray-100 font-normal', location.pathname === '/' ? 'text-base md:text-lg ' : 'text-sm md:text-lg' )}>{description}</p>
         </article>
-        {ctaText && ctaUrl && (
-                <Link to={ctaUrl}>
-                    <button type="button" className="button-class !h-11 !w-full md:w-[240px]">
-                        <img src="/assets/icons/plus.svg" alt="plus" className="size-5" />
-                        <span className="p-16-semibold text-white">{ctaText}</span>
-                    </button>
-                </Link>
-                
-        )}
     </header>
   )
 }

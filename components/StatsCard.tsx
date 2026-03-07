4641a1852959181543dcb6c5326b5564aca2d68e
ReaSetilo <202103018@ub.ac.bw>
2026-03-07 23:51:19 +0200

    mobile view
    fix trend graph on mobile

--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -35,7 +35,11 @@ const StatsCard = ({ headerTitle, total, ...props }: StatsCard) => {
               </p>
             </div>
 
-            <img src={`/assets/icons/${isDecrement ? 'decrement.svg' : 'increment.svg'} `} className="xl:w-32 w-full h-full md:h-32 xl:h-full" alt="trend graph" />
+            <img 
+  src={`/assets/icons/${isDecrement ? 'decrement.svg' : 'increment.svg'}`} 
+  className="w-full h-auto max-h-20 md:max-h-32 xl:w-32 xl:h-full xl:max-h-full" 
+  alt="trend graph" 
+/>
           </>
         )
       }
